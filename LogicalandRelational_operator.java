258228392d1dd9328860372443a14b920c124a0c
Gogul <gogul@Goguls-MacBook-Air.local>
2024-12-26 15:29:44 +0530

    Day 1 practice

---
+++ LogicalandRelational_operator.java
@@ -0,0 +1,11 @@
+public class LogicalandRelational_operator {
+    public static void main(String[] args){
+        int num1=10;
+        int num2=51;
+        if(num1>num2){
+            System.out.println("Num 1 is greater");
+        }else{
+            System.out.println("Num 2 is greater");
+        }
+    }
+}
---
+++ Primitive_conversion.java
@@ -0,0 +1,7 @@
+public class Primitive_conversion {
+    public static void main(String[] args){
+        float f=543.345f;
+        System.out.println(f);
+        System.out.println((int)(f));
+    }
+}
