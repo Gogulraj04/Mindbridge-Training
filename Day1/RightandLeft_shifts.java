236cc88e3ec7a98c3d94d1b5b01876283161e5b4
Gogul <gogul@Goguls-MacBook-Air.local>
2024-12-28 15:13:30 +0530

    Day1 task

---
+++ Day1/RightandLeft_shifts.java
@@ -0,0 +1,9 @@
+public class RightandLeft_shifts {
+    public static void main(String[] args){
+        int n1=9;
+        int n2=10;
+        System.out.println(n1<<n2);
+        System.out.println(n1^n2);
+    }
+
+}
---
+++ Day1/Ternary_operator.java
@@ -0,0 +1,7 @@
+public class Ternary_operator {
+    public static void main(String[] args){
+        int num=7;
+        String res=(num%2==0)?"Even":"Odd";
+        System.out.println(res);
+    }
+}
