b2ba599fb85d2295b8bc8dc571437cb72d295b82
Gogul <gogul@Goguls-MacBook-Air.local>
2025-01-06 00:58:24 +0530

    correction rectified

--- Day6/String/StringExample2.java
+++ Day6/String/StringExample2.java
@@ -8,12 +8,37 @@ public class StringExample2 {
         String s1=sc.nextLine();
         String s2=sc.nextLine();
 
-        System.out.println(s1.compareTo(s2));
-        System.out.println(s1.equals(s2));
-        System.out.println(s1==s2);
+        if(s1.compareTo(s2)<0){
+            System.out.println("first string lexicographically smaller than second string");
+        } else if (s1.compareTo(s2)>0) {
+            System.out.println("first string lexicographically greater than second string");
+        }else{
+            System.out.println("two strings lexicographically equal");
+        }
+
+        if(s1.equals(s2)){
+            System.out.println("Strings are exactly same {Case sensitive}");
+        }else{
+            System.out.println("Strings are not exactly same {Case sensitive}");
+        }
+
+        if (s1.equalsIgnoreCase(s2)) {
+            System.out.println("two strings equal {case-insensitive}");
+        } else {
+            System.out.println("two strings not equal {case-insensitive}");
+        }
+
+        if(s1==s2){
+            System.out.println("Both string reference are same");
+        }else{
+            System.out.println("Both string reference are not same");
+        }
+
 
         System.out.println(s1.concat(s2));
         System.out.println(s1+s2);
+
+
 
     }
 }
